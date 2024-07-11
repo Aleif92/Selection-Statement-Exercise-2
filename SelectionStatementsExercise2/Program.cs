c6ccffb8ac5603c03940162045ac2abbfee896a7
Alex Leifermann <aleif92@outlook.com>
2024-07-11 14:44:35 -0500

    Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            FavSubject();
+        }
+
+        public static void FavSubject()
+        {
+            Console.WriteLine("What is your favorite subject?");
+            string subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I do not prefer math");
+                    break;
+                case "science":
+                    Console.WriteLine("Science rules!");
+                    break;
+                case "pe" :
+                case "p.e" :
+                    Console.WriteLine("P.E is fun!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is my fav too");
+                    break;
+                default:
+                    Console.WriteLine("Thats not a class!");
+                    break;
+                    
+            }
         }
     }
 }
